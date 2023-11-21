4ff3805ca249c7312cc93bfb187bdccfaccb765a
jitujiten <jsahu5425@gmail.com>
2023-11-21 19:31:37 +0530

    "added new build with fixing signin & signup message error'

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const ProductsRouters = require("./routes/Products");
 const CategoriesRouters = require("./routes/Category");
@@ -34,7 +34,7 @@ opts.secretOrKey = process.env.JWT_SECREET_KEY;
 server.use(express.static(path.resolve(__dirname, "build")));
 
 server.use(cookieParser());
-server.use(bodyParser.json({ limit: '500kb' }));
+server.use(bodyParser.json({ limit: "10mb" }));
 
 server.use(
   session({
@@ -75,9 +75,8 @@ passport.use(
     try {
       const user = await User.findOne({ email: email }).exec();
       if (!user) {
-       return done(null, false, { message: "Invalid Credentials" });
+        return done(null, false, { message: "user not found" });
       }
-
       crypto.pbkdf2(
         password,
         user.salt,
@@ -99,8 +98,8 @@ passport.use(
             email: user.email,
             addresses: user.addresses,
             orders: user.orders,
-            name:user.name,
-            ProfileUrl:user.ProfileUrl
+            name: user.name,
+            ProfileUrl: user.ProfileUrl,
           });
         }
       );
@@ -122,8 +121,8 @@ passport.use(
           email: user.email,
           addresses: user.addresses,
           orders: user.orders,
-          name:user.name,
-          ProfileUrl:user.ProfileUrl
+          name: user.name,
+          ProfileUrl: user.ProfileUrl,
         }); //this calls serializer
       } else {
         return done(null, false);
@@ -137,18 +136,21 @@ passport.use(
 //this creates session variable req.user on being called from callbacks
 //take user send  id
 passport.serializeUser(function (user, cb) {
-  console.log("serialize");
-  process.nextTick(function () {
-    return cb(null, {
-      id: user.id,
-      role: user.role,
-      email: user.email,
-      addresses: user.addresses,
-      orders: user.orders,
-      name:user.name,
-      ProfileUrl:user.ProfileUrl
+  if (user.message) {
+    return cb(null, { message: user.message });
+  } else {
+    process.nextTick(function () {
+      return cb(null, {
+        id: user.id,
+        role: user.role,
+        email: user.email,
+        addresses: user.addresses,
+        orders: user.orders,
+        name: user.name,
+        ProfileUrl: user.ProfileUrl,
+      });
     });
-  });
+  }
 });
 
 //this creates session variable req.user when called from authorized request
@@ -169,25 +171,26 @@ server.post("/create-payment-intent", async (req, res) => {
   const { totalAmount, OrderId } = req.body;
 
   if (!OrderId || !totalAmount) {
-    return res.status(400).send({ error: "Missing orderId or totalAmount in the request body" });
+    return res
+      .status(400)
+      .send({ error: "Missing orderId or totalAmount in the request body" });
   }
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalAmount * 100,
-      currency: "inr",
-      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-      automatic_payment_methods: {
-        enabled: true,
-      },
-      metadata: {
-        OrderId,
-      },
-    });
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100,
+    currency: "inr",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    metadata: {
+      OrderId,
+    },
+  });
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
- 
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
 ///webhook
--- routes/Auth.js
+++ routes/Auth.js
@@ -6,7 +6,26 @@ const router = express.Router();
 
 router
   .post("/signup", createUser)
-  .post("/login", passport.authenticate("local"), loginUser)
+  .post(
+    "/login",
+    function (req, res, next) {
+      passport.authenticate("local", function (err, user, info) {
+        if (err) {
+          return res.status(400).json({ message: "Error during login" });
+        }
+        if (!user) {
+          // The 'info' object contains the message from passport.use
+          return res
+            .status(400)
+            .json({ message: info.message || "Invalid login credentials" });
+        }
+
+        req.user = user;
+        next();
+      })(req, res, next);
+    },
+    loginUser
+  )
   .get("/check",passport.authenticate("jwt"), checkAuth)
   .get("/logout", logout)
   .post("/reset-password-request",resetPasswordRequest)
